f8f51272554a644e4c4fb0ec61bccba5e35ef457
moondi <moon-di-2009@yandex.ru>
2022-08-17 17:45:08 +0300

    Реализовали вызов функции    repository.refreshTitle() через функцию высшего порядка.

--- start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -124,11 +125,11 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
     /**
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
-     fun refreshTitle() {
-        viewModelScope.launch {
+    private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
             try {
                 _spinner.value = true
-                repository.refreshTitle()
+                block()
             } catch (error: TitleRefreshError) {
                 _snackBar.value = error.message
             } finally {
@@ -136,4 +137,9 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
             }
         }
     }
+     fun refreshTitle() {
+         launchDataLoad {
+             repository.refreshTitle()
+         }
+     }
 }
